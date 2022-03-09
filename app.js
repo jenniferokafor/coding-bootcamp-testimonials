cc3b1d6bd3f835c8f94a7de59416e066578381ef
Jennifer Okafor <jennakafor00@gmail.com>
2022-03-09 17:29:10 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const testimonials = [
+    {
+        name: "Tanya Sinclair",
+        role: "UX Engineer",
+        quote: "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future.",
+        img: "image-tanya.jpg"
+    },
+
+    {
+        name: "John Tarkpor",
+        role: "Junior Front-end Developer",
+        quote: "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer.",
+        img: "/images/image-john.jpg"
+    }
+
+
+];
+
+// getting my elements
+const image = document.querySelector('.person-image');
+const name = document.querySelector('.name');
+const role = document.querySelector('.role');
+const quote = document.querySelector('.quote-text');
+
+const prevToggle = document.querySelector('.prev-toggle');
+const nextToggle = document.querySelector('.next-toggle');
+
+let counter = 0;
+let currentProfile = testimonials[counter];
+
+// setting up a variable to assign values
+assignProfile = () => {
+    // image.src = currentProfile.img;
+    name.textContent = currentProfile.name;
+    role.textContent = currentProfile.role;
+    quote.textContent = currentProfile.quote;
+}
+
+// setting up default screen 
+window.addEventListener('DOMContentLoaded', function () {
+ assignProfile();
+})
+
+// setting up previous button
+prevToggle.addEventListener('click', function () {
+    counter--;
+    if(counter < 0) {
+        counter = testimonials.length - 1;
+    }
+    assignProfile();
+    console.log('wtf');
+})
+
+// setting up next button
+nextToggle.addEventListener('click', function () {
+    counter++;
+    if(counter > testimonials.length - 1) {
+        counter = 0;
+    }
+    assignProfile();
+    console.log('Im alive');
+})
